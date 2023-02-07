ea6cdb390d487cec08494e18e4d6263edf8a117c
Mango2Juice <90748542+Mango2Juice@users.noreply.github.com>
2023-02-07 23:32:36 +0800

    Update config.py

--- Uploader/config.py
+++ Uploader/config.py
@@ -32,31 +32,41 @@ logging.basicConfig(
 )
 
 
-# sample config file 
-
 class Config(object):
-
-    # get a token from @BotFather
-    BOT_TOKEN = "6010449938:AAGzbzsRVn1h-1Jx6-TlImqp6tgyU3ZgTnU"
-    
+    WEBHOOK = os.environ.get("BOT_TOKEN", False)
+    # Get a token from @BotFather
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    # The Telegram API things
+    API_ID = int(os.environ.get("API_ID", 12345))
+    API_HASH = os.environ.get("API_HASH")
     # Get these values from my.telegram.org
-    API_ID = 25899147
-    API_HASH = "05a49e1d9dc7a622e52ad2b5081ede32"
-    
-    # No need to change
+    # Array to store users who are authorized to use the bot
+
+    # File /video download location
     DOWNLOAD_LOCATION = "./DOWNLOADS"
-    ADL_BOT_RQ = {}
-    CHUNK_SIZE = 128
+
+    MEGA_EMAIL = os.environ.get("MEGA_EMAIL", "None")
+    # If deploying on vps edit the above value as example := Mega_email = "Your-Mega_email-inside-inverted-commas."
+
+    # This is not necessary! Enter your mega password only if you have a mega.nz account with pro/business features.
+    MEGA_PASSWORD = os.environ.get("MEGA_PASSWORD", "None")
+    # If deploying on vps edit the above value as example := Mega_password = "Your-Mega_password-inside-inverted-commas."
+    # Telegram maximum file upload size
     TG_MAX_FILE_SIZE = 4194304000
-    HTTP_PROXY = ""
+
+    # Chunk size that should be used with requests
+    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
+    # Proxy for accessing youtube-dl in GeoRestricted Areas
+    # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
+    HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
+
+    # Set timeout for subprcess
     PROCESS_MAX_TIMEOUT = 3700
-    
-    # TG Ids
-    LOG_CHANNEL = -101619108747
-    OWNER_ID = 417770901
-    
-    # bot username without @
-    BOT_USERNAME = "TeleUploadBot"
-    
-    # auth users
-    AUTH_USERS = [OWNER_ID]
+
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "12356"))
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    ADL_BOT_RQ = {}
+    AUTH_USERS = list({int(x)
+                      for x in os.environ.get("AUTH_USERS", "0").split()})
+    AUTH_USERS.append(OWNER_ID)
